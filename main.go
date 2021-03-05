97c623a9550ef352d2302ee00d57e14700b5e98e
Becky Russoniello <rebecca.russoniello@gmail.com>
2021-03-04 23:19:58 -0600

    refactor into separate functions for the API call, printing info, and assembling IDs

--- main.go
+++ main.go
@@ -7,26 +7,39 @@ import (
 	"github.com/zmb3/spotify"
 )
 
-var releaseRadarId = "37i9dQZEVXblHXYINKqgaL"
+const releaseRadarId = "37i9dQZEVXblHXYINKqgaL"
+const releaseRadarLength = 30
 
-func printPlaylistInfo(client *spotify.Client) []spotify.PlaylistTrack {
+func getPlaylistInfo(client *spotify.Client) []spotify.PlaylistTrack {
 	page, err := client.GetPlaylistTracks(spotify.ID(releaseRadarId))
 	if err != nil {
 		log.Fatalf("couldn't get features playlists: %v", err)
 	}
 
+	return page.Tracks
+}
+
+func printPlaylistInfo(tracklist []spotify.PlaylistTrack) {
 	fmt.Printf("ID | Name | Popularity | Artists | Album | Duration")
-	for _, trackObj := range page.Tracks {
+	for _, trackObj := range tracklist {
 		track := trackObj.Track
 		fmt.Printf("%v | %v | %v | %v | %v | %v\n", track.ID, track.Name, track.Popularity, track.Artists[0].Name, track.Album.Name, track.TimeDuration())
 	}
+}
 
-	return page.Tracks
+func assembleTrackIDs(tracklist []spotify.PlaylistTrack) []spotify.ID {
+	var trackIDs = make([]spotify.ID, releaseRadarLength)
+	for _, trackObj := range tracklist {
+		trackIDs = append(trackIDs, trackObj.Track.ID)
+	}
+	return trackIDs
 }
 
 func main() {
 	fmt.Println("Ocotillo")
 	client := clientCredentialsAuth()
-	printPlaylistInfo(client)
-	//userAuth()
+	tracklist := getPlaylistInfo(client)
+	trackIDs := assembleTrackIDs(tracklist)
+	printPlaylistInfo(tracklist)
+	fmt.Println(trackIDs)
 }
