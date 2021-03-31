f60b574c6d26d91306ed6ed257498ed8139aad24
Becky Russoniello <rebecca.russoniello@gmail.com>
2021-03-31 01:03:10 -0500

    update stats tests

--- basic_stats_test.go
+++ basic_stats_test.go
@@ -31,37 +31,37 @@ func Test_sortByValence(t *testing.T) {
 }
 
 func Test_min(t *testing.T) {
-	vr := ValenceReport{songSlice: mockSongSlice()}
+	vr := StatReport{songSlice: mockSongSlice(), fieldName: "Valence"}
 	assertEqual(t, vr.min(), float32(1), "")
 }
 
 func Test_max(t *testing.T) {
-	vr := ValenceReport{songSlice: mockSongSlice()}
+	vr := StatReport{songSlice: mockSongSlice(), fieldName: "Valence"}
 	assertEqual(t, vr.max(), float32(21), "")
 }
 
 func Test_sum(t *testing.T) {
-	vr := ValenceReport{songSlice: mockSongSlice()}
+	vr := StatReport{songSlice: mockSongSlice(), fieldName: "Valence"}
 	assertEqual(t, vr.sum(), float32(33), "")
 }
 
 func Test_median(t *testing.T) {
-	vr := ValenceReport{songSlice: mockSongSlice()}
+	vr := StatReport{songSlice: mockSongSlice(), fieldName: "Valence"}
 	assertEqual(t, vr.median(), float32(11), "")
 	vr.songSlice = append(vr.songSlice, Song{Valence: 50})
 	assertEqual(t, vr.median(), float32(16), "")
 }
 
 func Test_mean(t *testing.T) {
-	vr := ValenceReport{songSlice: mockSongSlice()}
+	vr := StatReport{songSlice: mockSongSlice(), fieldName: "Valence"}
 	assertEqual(t, vr.mean(), float32(11), "")
 	vr.songSlice = append(vr.songSlice, Song{Valence: 39})
 	assertEqual(t, vr.mean(), float32(18), "")
 }
 
-func Example_printValenceReport() {
+func Example_printStatReport() {
 	playlistData := basicSongInfo()
-	printValenceReport(playlistData)
+	printStatReport(playlistData, "Valence")
 	// Output: Valence Report | Min: 0 | Max: 0 | Mean: 0 | Median: 0
 	// ID | Name | Artist | Album | Release Date | Danceability | Duration | Energy | Instrumentalness | Liveness | Popularity | Speechiness | Tempo | Valence
 	// hello 2 | Pictures Of You | The Cure | Disintegration | 5-2-1989 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
