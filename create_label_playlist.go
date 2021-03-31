aa003cb2ba30a54d8d621a3838712cce8ff955e0
Becky Russoniello <rebecca.russoniello@gmail.com>
2021-03-31 00:24:52 -0500

    create label playlist wip

---
+++ create_label_playlist.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"log"
+
+	"github.com/zmb3/spotify"
+)
+
+var userID = "1211800245"
+
+func createLabelPlaylist(labelName string, playlistType string) {
+	sp := SpotifyAPI{client: userAuth()}
+	title := fmt.Sprintf("%v Records", labelName)
+	if playlistType == "Top" {
+		title = title + " - Top Tracks"
+	}
+	// note: should handle error case below, don't just throw away
+	playlist, err := sp.client.CreatePlaylistForUser(userID, title, playlistDescription(labelName, playlistType), true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	allTracks := sp.getAllAlbumsByLabel(labelName)
+	trackChunks := assembleTrackIDs(*allTracks, 100)
+	for _, chunk := range trackChunks {
+		for i := 0; i < len(chunk); i += 100 { // max trackIDs to add to playlist in one req
+			trackIDsForReq := make([]spotify.ID, 0, 100)
+			maxJ := 100 + i
+			if len(chunk) < maxJ {
+				maxJ = len(chunk)
+			}
+			for j := 0 + i; j < maxJ; j++ {
+				trackIDsForReq = append(trackIDsForReq, chunk[j])
+			}
+			fmt.Println(playlist)
+			fmt.Println(trackIDsForReq)
+			sp.client.AddTracksToPlaylist(playlist.ID, trackIDsForReq...)
+		}
+	}
+
+}
+
+func playlistDescription(labelName string, playlistType string) string {
+	return fmt.Sprintf("%v releases from %v Records", playlistType, labelName)
+}
--- get_all_albums_by_label.go
+++ get_all_albums_by_label.go
@@ -9,35 +9,74 @@ import (
 
 var maxLimit int = 50
 
-const offsetMax int = 951
+const offsetMax int = 950
 
 var yearSpans = []string{"1900-1971", "1972-1986", "1987-2001", "2002-2011", "2012-2021"}
 
-func getAllAlbumsByLabel(recordLabelName string) {
-	client := clientCredentialsAuth()
+func (sp SpotifyAPI) getAllAlbumsByLabel(recordLabelName string) *SongSet {
+	//client := clientCredentialsAuth()
+	//var allTrackIDs [][]spotify.ID = make([][]spotify.ID, 0, 5)
+	var allTrackIDs SongSet = make(SongSet)
 	for yearSpansIndex := 0; yearSpansIndex < len(yearSpans); yearSpansIndex++ {
 		gotAllResults := false
+		years := yearSpans[yearSpansIndex]
+		var albumIDs []spotify.ID = make([]spotify.ID, 0, 1000)
+		//var trackIDs []spotify.ID = make([]spotify.ID, 0, 50000)
+
 		for offset := 0; offset < offsetMax && !gotAllResults; offset += maxLimit {
-			years := yearSpans[yearSpansIndex]
 			// search for albums from record label
-			results, err := client.SearchOpt(fmt.Sprintf("label:\"%v\" year:%v", recordLabelName, years), spotify.SearchTypeAlbum, &spotify.Options{Limit: &maxLimit, Offset: &offset})
+			results, err := sp.client.SearchOpt(fmt.Sprintf("label:\"%v\" year:%v", recordLabelName, years), spotify.SearchTypeAlbum, &spotify.Options{Limit: &maxLimit, Offset: &offset})
 			if err != nil {
 				log.Fatal(err)
 			}
-			printSearchResults(years, results)
+			printSearchResults(years, &albumIDs, results)
 			if results.Albums.Total <= (offset + maxLimit) {
 				gotAllResults = true
 			}
 		}
+
+		sp.getTracksForAlbums(&albumIDs, allTrackIDs)
+		/*for _, song := range *trackIDsForAlbums {
+			allTrackIDs[trackID] = Song{ ReleaseDate: }
+		}*/
+		//allTrackIDs = append(allTrackIDs, *trackIDsForAlbums)
 	}
 
+	sp.addAudioFeatures(allTrackIDs)
+	return &allTrackIDs
 }
 
-func printSearchResults(years string, results *spotify.SearchResult) {
-	// handle album results
+func (sp *SpotifyAPI) getTracksForAlbums(albumIDs *[]spotify.ID, trackInfo SongSet) SongSet {
+	var albumsPerRequest = 20
+	for offset := 0; offset < len(*albumIDs); offset += albumsPerRequest {
+		fmt.Println("In getTracksForAlbums. offset =", offset, "; len(*albumIDs) =", len(*albumIDs))
+		var endIndex = offset + albumsPerRequest
+		if len(*albumIDs) < endIndex {
+			endIndex = len(*albumIDs)
+		}
+		var albumIDsForReq = (*albumIDs)[offset:endIndex]
+		var albumsData, _ = sp.client.GetAlbumsOpt(&spotify.Options{
+			Limit:  &albumsPerRequest,
+			Offset: &offset,
+		}, albumIDsForReq...)
+		fmt.Println(len(albumIDsForReq), "albums requested.", len(albumsData), "albums received.")
+		for _, album := range albumsData {
+			fmt.Println("Album", album.Name, "has", len(album.Tracks.Tracks), "tracks.")
+			for _, track := range album.Tracks.Tracks {
+				//*trackIDs = append(*trackIDs, track.ID)
+				trackInfo[track.ID] = Song{ReleaseDate: album.ReleaseDate}
+				//fmt.Println(track.ID, album.ReleaseDate)
+			}
+		}
+	}
+	return trackInfo
+}
+
+func printSearchResults(years string, albumIDs *[]spotify.ID, results *spotify.SearchResult) {
 	if results.Albums != nil {
 		fmt.Println("Albums (", years, "):")
 		for _, item := range results.Albums.Albums {
+			*albumIDs = append(*albumIDs, item.ID)
 			fmt.Println("   ", item.Artists[0].Name, "-", item.Name, " || ", item.ReleaseDate)
 		}
 	}
